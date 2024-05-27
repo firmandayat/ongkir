744b2d9a1dd522713dfc03c822c107d33a77474a
firmandayat <firman.txt@gmail.com>
2024-05-27 19:41:12 +0700

    tes

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(ProvinceSeeder::class);
+        $this->call(CitySeeder::class);
+        $this->call(CourierSeeder::class);
+    }
+}
